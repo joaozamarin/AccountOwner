81a57319cbd29a2d3fefb8c5e1ac12b2de0ff03f
joaozamarin <joao.zamarin@gmail.com>
2023-03-03 22:27:22 -0300

    Criação do projeto entidade e classe de conexão com o banco

--- backend/AccountOwnerServer/Extensions/ServiceExtensions.cs
+++ backend/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 using Contracts;
 using LoggerService;
+using Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccountOwnerServer.Extensions
 {
@@ -27,6 +29,14 @@ namespace AccountOwnerServer.Extensions
         public static void ConfigureLoggerService(this IServiceCollection services)
         {
             services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var conn = config["mysqlconnection:connectionString"];
+
+            services.AddDbContext<RepositoryContext>(options => 
+                options.UseMySql(conn, ServerVersion.AutoDetect(conn)));
         }
     }
 }
