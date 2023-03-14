75d9580f23f0de12d447e45a4c94e1e8d31c0d2a
joaozamarin <joao.zamarin@gmail.com>
2023-03-14 20:30:53 -0300

    Criação dos repositórios

---
+++ backend/Contracts/IAccountRepository.cs
@@ -0,0 +1,9 @@
+using Entities.Models;
+
+namespace Contracts
+{
+    public interface IAccountRepository : IRepositoryBase<Account>
+    {
+        
+    }
+}
--- backend/Entities/RepositoryContext.cs
+++ backend/Entities/RepositoryContext.cs
@@ -9,7 +9,7 @@ namespace Entities
         {
         }
 
-        public DbSet<Owner>? Owners { get; set; }
-        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Account> Accounts { get; set; }
     }
 }
