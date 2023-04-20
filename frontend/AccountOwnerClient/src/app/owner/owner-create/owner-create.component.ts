57a1a394120b4db17a49210eda4f05142ba21263
joaozamarin <joao.zamarin@gmail.com>
2023-04-20 20:26:52 -0300

    create ok

--- frontend/AccountOwnerClient/src/app/owner/owner-create/owner-create.component.ts
+++ frontend/AccountOwnerClient/src/app/owner/owner-create/owner-create.component.ts
@@ -1,7 +1,11 @@
+import { Owner } from './../../_interfaces/owner.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SuccessModalComponent } from './../../shared/modals/success-modal/success-modal.component';
+import { OwnerForCreation } from './../../_interfaces/ownerForCreation.model';
 import { Router } from '@angular/router';
 import { ErrorHandlerService } from './../../shared/services/error-handler.service';
 import { OwnerRepositoryService } from './../../shared/services/owner-repository.service';
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
@@ -11,7 +15,7 @@ import { DatePipe } from '@angular/common';
   templateUrl: './owner-create.component.html',
   styleUrls: ['./owner-create.component.scss']
 })
-export class OwnerCreateComponent implements OnInit{
+export class OwnerCreateComponent implements OnInit {
   errorMessage: string = '';
   ownerForm: FormGroup;
   bsModalRef?: BsModalRef;
@@ -21,8 +25,8 @@ export class OwnerCreateComponent implements OnInit{
     private errorHandler: ErrorHandlerService,
     private router: Router,
     private datePipe: DatePipe,
-    private modal: BsModalRef
-  ) {}
+    private modal: BsModalService
+  ) { }
 
   ngOnInit(): void {
     this.ownerForm = new FormGroup({
@@ -30,5 +34,58 @@ export class OwnerCreateComponent implements OnInit{
       dateOfBirth: new FormControl('', [Validators.required]),
       address: new FormControl('', [Validators.required, Validators.maxLength(100)])
     });
+  }
+
+  validateControl(controlName: string) {
+    if (this.ownerForm.get(controlName).invalid && this.ownerForm.get(controlName).touched)
+      return true;
+
+    return false;
+  }
+
+  hasError(controlName: string, errorName: string) {
+    if (this.ownerForm.get(controlName).hasError(errorName))
+      return true;
+
+    return false;
+  }
+
+  createOwner(ownerFormValue) {
+    if (this.ownerForm.valid)
+      this.executeOwnerCreation(ownerFormValue);
+  }
+
+  private executeOwnerCreation(ownerFormValue) {
+    const owner: OwnerForCreation = {
+      name: ownerFormValue.name,
+      dateOfBirth: this.datePipe.transform(ownerFormValue.dateOfBirth, 'yyyy-MM-dd'),
+      address: ownerFormValue.address
+    }
+
+    const apiUrl = 'api/owner';
+
+    this.repository.createOwner(apiUrl, owner)
+      .subscribe({
+        next: (own: Owner) => {
+          const config: ModalOptions = {
+            initialState: {
+              modalHeaderText: 'Success Message',
+              modalBodyText: `Owner: ${own.name} created successfully`,
+              okButtonText: 'OK'
+            }
+          };
+          this.bsModalRef = this.modal.show(SuccessModalComponent, config);
+          this.bsModalRef.content.redirectOnOk.subscribe(_ => this.redirectToOwnerList());
+        },
+        error: (err: HttpErrorResponse) => {
+          this.errorHandler.handleError(err);
+          this.errorMessage = this.errorHandler.errorMessage;
+        }
+      }
+      )
+  }
+
+  redirectToOwnerList() {
+    this.router.navigate(['/owner/list']);
   }
 }
--- frontend/AccountOwnerClient/src/app/shared/services/error-handler.service.ts
+++ frontend/AccountOwnerClient/src/app/shared/services/error-handler.service.ts
@@ -1,3 +1,5 @@
+import { ErrorModalComponent } from './../modals/error-modal/error-modal.component';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
@@ -8,7 +10,7 @@ import { Router } from '@angular/router';
 export class ErrorHandlerService {
   public errorMessage: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private modal: BsModalService) { }
 
   public handleError = (error: HttpErrorResponse) => {
     if (error.status === 500) {
@@ -21,17 +23,29 @@ export class ErrorHandlerService {
       this.handleOtherError(error);
     }
   }
+
   private handle500Error = (error: HttpErrorResponse) => {
     this.createErrorMessage(error);
     this.router.navigate(['/500']);
   }
+
   private handle404Error = (error: HttpErrorResponse) => {
     this.createErrorMessage(error);
     this.router.navigate(['/404']);
   }
+
   private handleOtherError = (error: HttpErrorResponse) => {
-    this.createErrorMessage(error); //TODO: this will be fixed later;
+    this.createErrorMessage(error);
+    const config: ModalOptions = {
+      initialState: {
+        modalHeaderText: 'Error Message',
+        modalBodyText: this.errorMessage,
+        okButtonText: 'OK'
+      }
+    };
+    this.modal.show(ErrorModalComponent, config);
   }
+
   private createErrorMessage = (error: HttpErrorResponse) => {
     this.errorMessage = error.error ? error.error : error.statusText;
   }
